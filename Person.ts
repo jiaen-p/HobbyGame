6cfdad4e30f2e0095a4faf3a455998c5518de42a
Jiaen Pan <jiaen.m.pan@gmail.com>
2020-05-20 13:14:12 +0200

    terminado clase persona y su test

---
+++ Person.ts
@@ -0,0 +1,52 @@
+export class Person{
+    private name: string
+    private nationality: string
+    private profession: string
+    private experienceYears: number
+    constructor(name: string, nationality: string, profession: string, experienceYears: number){
+        this.name = name
+        this.nationality = nationality
+        this.profession = profession
+        this.experienceYears = experienceYears
+    }
+    public samePerson(p:Person):boolean{
+        return this.name === p.getName() && this.nationality === p.getNationality() && this.profession === p.getProfession() && this.experienceYears === p.getExperienceYears()
+    }
+    public print(){
+        console.log(`
+        Nombre: ${this.name}
+        Nacionalidad: ${this.nationality}
+        Profesion: ${this.profession}
+        Años de experiencia: ${this.experienceYears}
+        `)
+    }
+    public toString():string{
+        let s:string = ''
+        s = this.name + '\n' + this.nationality + '\n' + this.profession + '\n' + this.experienceYears + '\n'
+        return s
+    }
+    public getName():string{
+        return this.name
+    }
+    public setName(name:string){
+        this.name = name
+    }
+    public getNationality():string{
+        return this.nationality
+    }
+    public setNationality(nationality:string){
+        this.nationality = nationality
+    }
+    public getProfession():string{
+        return this.profession
+    }
+    public setProfession(profession:string){
+        this.profession = profession
+    }
+    public getExperienceYears():number{
+        return this.experienceYears
+    }
+    public setExperienceYears(experienceYears:number){
+        this.experienceYears = experienceYears
+    }
+}
